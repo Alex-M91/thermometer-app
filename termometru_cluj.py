21258ac70039c1c0428fac0805ed37f642842ca7
Alex Mihaescu <alexcugir_ps4@yahoo.com>
2019-11-16 12:03:14 +0200

    Add logic to thermometer class methods

--- termometru_cluj.py
+++ termometru_cluj.py
@@ -1,16 +1,20 @@
-class Themperature(Cluj):
-    def __init__(self, themperature = 0):
-        self.themperature = themperature
+class Thermometer():
+    def __init__(self, temperature = 0):
+        self.temperature = temperature
 
-    def get_themperature(self, instance, owner):
-        return self.themperature
+    def get_temperature(self, unit="celsius"):
+        if unit=="celsius":
+            return self.temperature
+        elif unit=="fahrenheit":
+            return self.to_fahrenheit(self.temperature)
 
-    def set_themperature(self, instance, themperature):
-        return self.themperature
+    def set_temperature(self, temperature):
+        self.temperature = temperature
 
+    def to_celsius(self, temperature):
+        temperature = (temperature - 32) * 5 / 9
+        return temperature
 
-    def to_celsius(self):
-        pass
-
-    def to_fahrenheit(self):
-        pass
+    def to_fahrenheit(self, temperature):
+        temperature = temperature * 1.8 + 32
+        return temperature
